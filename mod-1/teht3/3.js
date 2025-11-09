4ec7695e131127ae0833e2914d6b76da3c8e5f2a
pullavaras <linneagodunov@protonmail.com>
2025-11-09 13:06:53 +0200

    -

--- mod-1/teht3/3.js
+++ mod-1/teht3/3.js
@@ -1,1 +1,10 @@
-const numero = +numeroSrt;
+
+const numero1 = prompt('Anna 1. kokonaisluku: ')
+const numero2 = prompt('Anna 2, kokonaisluku:')
+const numero3 = prompt('Anna 3. kokonaisluku:')
+
+sum = numero1 + numero2 + numero3
+product = numero1 * numero2 * numero3
+avg = sum / 3
+
+document.querySelector("#target").innerHTML = "summa: " + sum + "luvut kerrottuna keskenään: " + product + "Keskiarvo: " + avg;
