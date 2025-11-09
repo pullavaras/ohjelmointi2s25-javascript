f7a424f90abbdda76072d7acd0905ec3b73d867d
pullavaras <linneagodunov@protonmail.com>
2025-11-09 13:10:52 +0200

    -

--- mod-1/teht3/3.js
+++ mod-1/teht3/3.js
@@ -3,7 +3,7 @@ const numero1 = prompt('Anna 1. kokonaisluku: ')
 const numero2 = prompt('Anna 2, kokonaisluku:')
 const numero3 = prompt('Anna 3. kokonaisluku:')
 
-sum = numero1 + numero2 + numero3
+const sum = parseFloat(numero1 + numero2 + numero3)
 product = numero1 * numero2 * numero3
 avg = sum / 3
 
