fe815c3763987de2eae7a91aab2f506e878cb3a9
pullavaras <linneagodunov@protonmail.com>
2025-11-22 19:21:04 +0200

    -

--- mod-1/teht4/4.js
+++ mod-1/teht4/4.js
@@ -1,5 +1,22 @@
 'use strict';
 const student = prompt("Kerro nimesi: ")
 
-const sorting_hat = Math.random(1, 4)
+function sorting_hat (min, max) {
+  const minCeiled = Math.ceil(1);
+  const maxFloored = Math.floor(4);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
 
+const student_house = sorting_hat()
+if (student_house == 1) {
+  document.querySelector("#target").innerHTML = student + ", you are a Gryffindor."
+}
+else if (student_house == 2) {
+  document.querySelector("#target").innerHTML = student + ", you are a Slytherin."
+}
+else if (student_house == 3) {
+  document.querySelector("#target").innerHTML = student + ", you are a Hufflepuff."
+}
+else {
+  document.querySelector("#target").innerHTML = student + ", you are a Ravenclaw."
+}
